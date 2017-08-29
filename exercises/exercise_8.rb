171c79b99127b53215af3855cdef90aa769672ab
John Goff <gygn.johngoff@gmail.com>
2017-08-29 16:11:38 -0400

    exercise 8

---
+++ exercises/exercise_8.rb
@@ -0,0 +1,14 @@
+require_relative '../setup'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
+require_relative './exercise_7'
+
+puts "Exercise 8"
+puts "----------"
+
+pers = @store1.employees.create(first_name: 'Bob', last_name: 'Smith', hourly_rate: 60)
+p pers.password
--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,10 @@ class Employee < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, inclusion: {in: 40..200}
+  
+  before_create :new_password
+  private
+  def new_password
+    self.password = (0...8).map { ('a'..'z').to_a[rand(26)] }.join
+  end
 end
