307ca6818f3e52b5e5f7107bae7a23a7b2f8807a
John Goff <gygn.johngoff@gmail.com>
2017-08-29 17:11:47 -0400

    exercise 9

--- lib/store.rb
+++ lib/store.rb
@@ -4,10 +4,20 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
   validate :mens_or_womens_or_both
 
+  before_destroy :validate_destroy
+
+  private
   def mens_or_womens_or_both
     if !mens_apparel && !womens_apparel
       errors.add(:mens_apparel, "Store must have either mens or womens apparel")
       errors.add(:womens_apparel, "Store must have either mens or womens apparel")
     end
+  end
+
+  def validate_destroy
+    p self.employees.size
+    return true if self.employees.size == 0
+    errors.add(:employees, "must be empty!")
+    throw(:abort)
   end
 end
